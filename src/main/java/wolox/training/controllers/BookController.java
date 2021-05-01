8291b71b7e5a7bc8cfd9baef69103ceb3bf85dbb
Steven Lotero Mesa <steven.lotero@wolox.co>
2021-04-30 22:03:54 -0500

    feat Javadoc for repository and book controller methods

--- src/main/java/wolox/training/controllers/BookController.java
+++ src/main/java/wolox/training/controllers/BookController.java
@@ -24,28 +24,58 @@ public class BookController {
     @Autowired
     BookRepository bookRepository;
 
+    /**
+     * Get all the books registered
+     *
+     * @return all the books registered
+     */
     @GetMapping
     public Iterable findAll(){
         return bookRepository.findAll();
     }
 
+    /**
+     * Find a book by its id
+     *
+     * @param id: Book identifier (Long)
+     * @return the book corresponding to the requested id
+     */
     @GetMapping("/{id}")
     public Book findOne(@PathVariable Long id){
         return bookRepository.findById(id).orElseThrow(BookNotFoundException::new);
     }
 
+    /**
+     * Create a book record
+     *
+     * @param book: Book to be created (Book)
+     * @return
+     */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Book create(@RequestBody Book book){
         return bookRepository.save(book);
     }
 
+    /**
+     * Delete a book by its ID
+     *
+     * @param id: Book identifier (Long)
+     */
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete (@PathVariable Long id){
         findOne(id);
         bookRepository.deleteById(id);
     }
+
+    /**
+     * Update a book by its ID
+     *
+     * @param book: Book to be updated (Book) RequestBody
+     * @param id: Book identifier (Long) PathVariable
+     * @return
+     */
 
     @PutMapping("/{id}")
     public Book updateBook(@RequestBody Book book, @PathVariable Long id){
@@ -56,6 +86,13 @@ public class BookController {
         return bookRepository.save(book);
     }
 
+    /**
+     * Greetings! with a name
+     *
+     * @param name: Optional name of who is going to greet (String)
+     * @param model: Contains the data that appears in the view (Model)
+     * @return The name of the view to perform the greeting
+     */
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required = false,
             defaultValue = "World") String name, Model model){
--- src/main/java/wolox/training/repositories/BookRepository.java
+++ src/main/java/wolox/training/repositories/BookRepository.java
@@ -7,6 +7,13 @@ import wolox.training.models.Book;
 
 @Repository
 public interface BookRepository extends CrudRepository<Book, Long> {
+
+    /**
+     * Find a book by its author's name
+     *
+     * @param author: Name of the author of the book to find (String)
+     * @return First book with the author's name
+     */
     Optional<Book> findFirstByAuthor(String author);
 }
 
