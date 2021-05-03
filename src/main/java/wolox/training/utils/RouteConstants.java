36464387e4c03ec06eda19c20aa7c39ba782336b
Steven Lotero Mesa <steven.lotero@wolox.co>
2021-05-03 13:53:49 -0500

    Squashed commit of the following:
    commit 047b2de66b3317eb1f679a6aa95283607a8e6516
    Merge: b054499 0d3dbc6
    Author: Steven Lotero Mesa <steven.lotero@wolox.co>
    Date:   Mon May 3 13:48:45 2021 -0500
    
        Squashed commit of the following:
    
        commit 0d3dbc66979a87cc7d4ff75a30555e98b2d5b5f2
        Author: Steven Lotero Mesa <steven.lotero@wolox.co>
        Date:   Mon May 3 13:36:07 2021 -0500
    
            fix changed the variable name bookId to id
    
        commit 159f0b20f56af9aca772480552bb8dacdc50d95d
        Author: Steven Lotero Mesa <steven.lotero@wolox.co>
        Date:   Mon May 3 13:23:43 2021 -0500
    
            fix the constants were extracted into two new files MessageError and RouteConstants
    
    commit b05449926a6897326a5c16ced84e4a1a951b575f
    Author: Steven Lotero Mesa <steven.lotero@wolox.co>
    Date:   Mon May 3 13:43:14 2021 -0500
    
        fix changed the variable name bookId to id
    
    commit 3ef000a908c78e9d38258ad7090730656dcff804
    Author: Steven Lotero Mesa <steven.lotero@wolox.co>
    Date:   Mon May 3 13:40:59 2021 -0500
    
        Squashed commit of the following:
    
        commit 0d3dbc66979a87cc7d4ff75a30555e98b2d5b5f2
        Author: Steven Lotero Mesa <steven.lotero@wolox.co>
        Date:   Mon May 3 13:36:07 2021 -0500
    
            fix changed the variable name bookId to id
    
        commit 159f0b20f56af9aca772480552bb8dacdc50d95d
        Author: Steven Lotero Mesa <steven.lotero@wolox.co>
        Date:   Mon May 3 13:23:43 2021 -0500
    
            fix the constants were extracted into two new files MessageError and RouteConstants
    
    commit 0d3dbc66979a87cc7d4ff75a30555e98b2d5b5f2
    Author: Steven Lotero Mesa <steven.lotero@wolox.co>
    Date:   Mon May 3 13:36:07 2021 -0500
    
        fix changed the variable name bookId to id
    
    commit b6564c4863278dd9c5883dd88bf9162ce37bb14d
    Author: Steven Lotero Mesa <steven.lotero@wolox.co>
    Date:   Mon May 3 13:26:37 2021 -0500
    
        Squashed commit of the following:
    
        commit 159f0b20f56af9aca772480552bb8dacdc50d95d
        Author: Steven Lotero Mesa <steven.lotero@wolox.co>
        Date:   Mon May 3 13:23:43 2021 -0500
    
            fix the constants were extracted into two new files MessageError and RouteConstants
    
    commit 159f0b20f56af9aca772480552bb8dacdc50d95d
    Author: Steven Lotero Mesa <steven.lotero@wolox.co>
    Date:   Mon May 3 13:23:43 2021 -0500
    
        fix the constants were extracted into two new files MessageError and RouteConstants
    
    commit 8ef4ea39ff6a0ff9912e2be911d4cb8a9506e821
    Author: Steven Lotero Mesa <steven.lotero@wolox.co>
    Date:   Mon May 3 10:38:14 2021 -0500
    
        fix missing returns were added in javadoc of bookController
    
    commit c6e7bc720b9872fe1855119801f5abd042b18d9c
    Merge: 5ba7c06 f3c8254
    Author: Steven Lotero Mesa <steven.lotero@wolox.co>
    Date:   Mon May 3 10:29:15 2021 -0500
    
        Merge branch 'feature/book-crud' into feature/book-java-doc
    
    commit f3c82545b1dac8760a6699a0290018dbff966c01
    Author: Steven Lotero Mesa <steven.lotero@wolox.co>
    Date:   Mon May 3 10:27:29 2021 -0500
    
        fix extract messages from exceptions into constants

---
+++ src/main/java/wolox/training/utils/RouteConstants.java
@@ -0,0 +1,10 @@
+package wolox.training.utils;
+
+public class RouteConstants {
+
+    public static final String BOOK_CONTROLLER_BASE_PATH = "/api/books";
+    public static final String PATH_VARIABLE_BOOK_ID = "/{id}";
+
+    private RouteConstants() {
+    }
+}
